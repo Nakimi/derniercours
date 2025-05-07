bc05450728445050c7bcb0422b8eac2719332f0e
eva87-pixel <cruveillier.eva@educ-valadon-limoges.fr>
2025-05-07 17:23:07 +0200

    Update note.js
    doc

--- src/models/note.js
+++ src/models/note.js
@@ -1,12 +1,13 @@
-
+// On importe Schema et model depuis mongoose
 import { Schema, model } from "mongoose";
 
-
+// On définit la structure des données pour une note d'élève
 const NoteSchema = new Schema({
-    id_note: { type: Number, required: true },
-    note: { type: Number, required: true },
-    id_matiere: { type: Schema.Types.ObjectId, required: true },
-    id_eleve: { type: Schema.Types.ObjectId, required: true }
+    id_note: { type: Number, required: true },                                   // Identifiant unique de la note
+    note: { type: Number, required: true },                                      // Valeur de la note
+    id_matiere: { type: Schema.Types.ObjectId, ref: "Matiere", required: true }, // Référence à la matière
+    id_eleve: { type: Schema.Types.ObjectId, ref: "Eleves", required: true }     // Référence à l'élève
 });
 
+// On crée et exporte le modèle "Note" basé sur ce schéma
 export default model("Note", NoteSchema);
