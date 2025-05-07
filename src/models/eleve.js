59329e62486f49ceacd598486a4d613e9697ea6a
eva87-pixel <cruveillier.eva@educ-valadon-limoges.fr>
2025-05-07 17:18:47 +0200

    Update eleve.js

--- src/models/eleve.js
+++ src/models/eleve.js
@@ -3,9 +3,9 @@ import { Schema, model } from "mongoose";
 
 //On définit la structure des données pour un élève
 const ElevesSchema = new Schema({
-    nom_eleve: { type: String, required: true },
-    prenom_eleve: { type: String, required: true },
-    moyenne_gen_eleve: { type: Number, default: 0 }
+    nom_eleve: { type: String, required: true }, //le nom est obligatoire
+    prenom_eleve: { type: String, required: true }, //le prénom est obligatoire
+    moyenne_gen_eleve: { type: Number, default: 0 } //Moyenne gen, par défaut 0
 });
 
 //On crée et exporte le modèle "Eleves" basé sur ce schéma
